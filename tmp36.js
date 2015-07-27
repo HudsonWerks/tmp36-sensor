e61ecc7ba9d1be2ed43283f762f61d2fe404d193
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2015-07-27 13:09:44 -0400

    Update tmp36.js

--- tmp36.js
+++ tmp36.js
@@ -13,22 +13,11 @@ function readTMP(){
     b.analogRead(tempPin, printTemp);
 }
 
-// function printTemp(aRead) {
-//     console.log("Getting temperature...");
-//     var x = (aRead.value * 1800/1024);
-
 function printTemp(x) {
     console.log("Getting temperature for Ground Control...");
     var millivolts = x.value * 1800; //1.8V data
     var temp_c = (millivolts - 500) /10;
     var temp_f = (temp_c *9/5) + 32;
-//    console.log("value " + aRead.value);
-//    console.log("x:  " + x);
-
-    // var cel = 100*x -50;
-    // var fah = (cel *9/5)+32;
-    // var temp_c = 100*x -50;
-    // var temp_f = (temp_c *9/5) + 32;
     console.log("Fahrenheit:  " + temp_f);
     console.log("Celsius:  " + temp_c);
     console.log("----------");
