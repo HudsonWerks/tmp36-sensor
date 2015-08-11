bc3f0feb1897487decd0d953370a870fffce8552
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2015-08-11 14:34:45 -0400

    Update tmp36.js

--- tmp36.js
+++ tmp36.js
@@ -1,12 +1,12 @@
- //Setup
-var b = require('bonescript'); // Read library
+//Setup
+var b = require('bonescript'); // Read bonescript library
 var tempPin = 'P9_38'; // Pin location for temperature sensor
+
+console.log("Sensor reading started.");
+console.log("Output at 10-sec intervals.");
 
 //Check the temperature every 10 seconds. Adjust according to your preferred intervals.
 setInterval(readTMP, 10000);
-
-console.log("Reading started.");
-console.log("Output at 10-sec Intervals.");
 
 //Define the 'readTMP' function
 function readTMP(){
@@ -14,11 +14,13 @@ function readTMP(){
 }
 
 function printTemp(x) {
-    console.log("Getting temperature for Ground Control...");
-    var millivolts = x.value * 1800; //1.8V data
-    var temp_c = (millivolts - 500) /10;
-    var temp_f = (temp_c *9/5) + 32;
-    console.log("Fahrenheit:  " + temp_f);
-    console.log("Celsius:  " + temp_c);
-    console.log("----------");
+	var timeNow = new Date();
+	var UTC = timeNow.toUTCString();
+	console.log("Temperature at Ground Control for " + UTC);
+	var millivolts = x.value * 1800;
+	var temp_c = (millivolts - 500) / 10;
+	var temp_f = (temp_c *9/5) + 32;
+	console.log("Fahrenheit: " + temp_f);
+	console.log("Celsius: " + temp_c);
+	console.log("_____________");
 }
